7e94927ebb59a10b76688e7c8cf2c087c7c5462c
Eliel Assis <eliel.assis7@hotmail.com>
2019-08-14 22:58:25 -0300

    inicio de projeto

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+
+
+namespace Sales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"D:\Projetos Visual Studio\Sales\Sale.csv";
+            string targetPath = @"D:\Projetos Visual Studio\Sales\Summary.csv";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(sourcePath))
+                {
+                    Sale[] mat = new Sale[(int)sr.BaseStream.Length];
+                    int n = 0;
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string product = line[0];
+                        double price = double.Parse(line[1], CultureInfo.InvariantCulture);
+                        int qty = int.Parse(line[2]);
+                        mat[n] = new Sale(product, price, qty);
+                        Console.WriteLine(mat[n].ToString());
+
+                        using (StreamWriter sw = File.AppendText(targetPath))
+                        {
+                            sw.WriteLine(mat[n]);
+                        }
+                        n++;
+                    }
+                }
+
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Sale.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+
+namespace Sales
+{
+    class Sale
+    {
+        public String Product { get; set; }
+        public double Price { get; set; }
+        public int Quantaty { get; set; }
+
+        public Sale(string product, double price, int quantaty)
+        {
+            Product = product;
+            Price = price;
+            Quantaty = quantaty;
+        }
+
+        public double TotalPrice()
+        {
+            return Price * Quantaty;
+        }
+
+        public override string ToString()
+        {
+            return Product + ": " + TotalPrice().ToString("C2");
+        }
+    }
+}
